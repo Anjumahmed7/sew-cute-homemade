2df999c4761dcae86c08d145b9569c5947d9867e
Ahmed Anjum <ahmedanjum2018@gmail.com>
2025-09-05 17:21:21 -0500

    Removed phone number / set to None

--- client/src/config/contact.js
+++ client/src/config/contact.js
@@ -1,7 +1,7 @@
 const CONTACT = {
   shopName: "Sew Cute Homemade",
   email: "sewcutehome131@gmail.com",
-  phone: "+1 (204) 392-3024",
+  phone: "+1 (000) 000-0000",
   facebook: "https://www.facebook.com/profile.php?id=61577007540479",
   instagram: "https://www.instagram.com/sewcutehomemade",
 };
